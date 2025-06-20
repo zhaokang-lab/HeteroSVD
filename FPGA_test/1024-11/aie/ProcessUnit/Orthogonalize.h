77e31580fcf3a5a13e680ed9465b4f08d5ae18d9
Xinya Luan <76553203+lxy-littlechai@users.noreply.github.com>
2025-06-20 11:32:13 +0800

    Delete FPGA_test/1024-11 directory

--- FPGA_test/1024-11/aie/ProcessUnit/Orthogonalize.h
+++
@@ -1,7 +0,0 @@
-#include "adf.h"
-
-template<int ROW>
-void orthogonalize(adf::input_buffer<float, adf::extents<1+ROW>>& in0, 
-                   adf::input_buffer<float, adf::extents<1+ROW>>& in1, 
-                   adf::output_buffer<float, adf::extents<1+ROW>>& out0, 
-                   adf::output_buffer<float, adf::extents<1+ROW>>& out1);
