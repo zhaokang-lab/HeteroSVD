8941d86956facbdda1792a17aa3b225de106537b
Xinya Luan <76553203+lxy-littlechai@users.noreply.github.com>
2025-06-20 11:32:26 +0800

    Delete FPGA_test/128-11 directory

--- FPGA_test/128-11/SystemConfig.h
+++
@@ -1,5 +0,0 @@
-const int ROW = 128;
-const int COL = 132;
-const int BLOCK_SIZE = 11;
-const int BLOCK_NUM = COL/BLOCK_SIZE;
-const float EPS = 1e-6;
--- FPGA_test/128-11/aie/ProcessUnit/Normalize.h
+++
@@ -1,5 +0,0 @@
-#include "adf.h"
-
-template<int ROW>
-void normalize(adf::input_buffer<float, adf::extents<1+ROW>>& __restrict in, 
-                adf::output_buffer<float, adf::extents<1+ROW>>& __restrict out);
